5be8f208d53100691cfab9ce6c7f48dc3ce78a40
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2023-05-11 22:22:24 +0200

    stash and shelve has been practiced

---
+++ src/main/java/com/cydeo/cihat/ThisClassForStash.java
@@ -0,0 +1,7 @@
+package com.cydeo.cihat;
+
+public class ThisClassForStash {
+    public static void main(String[] args) {
+        System.out.println("Hello Stash Again");
+    }
+}
---
+++ src/main/java/com/cydeo/cihat/ThisClassJustForStash.java
@@ -0,0 +1,8 @@
+package com.cydeo.cihat;
+
+public class ThisClassJustForStash {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Stash");
+    }
+}
