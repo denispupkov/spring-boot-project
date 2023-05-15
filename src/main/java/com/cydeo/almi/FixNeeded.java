ae22e2edc55636bb578684f93ce63a8b7b2fe4a1
Almi's PC <almimeci@gmail.com>
2023-05-15 14:12:38 -0500

    fixed

--- src/main/java/com/cydeo/almi/FixNeeded.java
+++ src/main/java/com/cydeo/almi/FixNeeded.java
@@ -3,6 +3,6 @@ package com.cydeo.almi;
 public class FixNeeded {
     public static void main(String[] args) {
         //todo the next call needs to be fixed by adding your name to the beginning instead of dotS.
-        System.out.println(".... good morning and have a nice week..");
+        System.out.println("Almi good morning and have a nice week..");
     }
 }
