eb11366f585c9e1ab8d3e2819d7fb68fbbfd8e93
Kemail-terminal <sulkemref@gmail.com>
2023-05-15 12:20:09 -0500

    fix is done

--- src/main/java/com/cydeo/kemail/FixNeeded.java
+++ src/main/java/com/cydeo/kemail/FixNeeded.java
@@ -3,6 +3,6 @@ package com.cydeo.kemail;
 public class FixNeeded {
     public static void main(String[] args) {
         //todo the next call needs to be fixed by adding your name to the beginning instead of dotS.
-        System.out.println(".... good morning and have a nice week..");
+        System.out.println("Kemal good morning and have a nice week..");
     }
 }
