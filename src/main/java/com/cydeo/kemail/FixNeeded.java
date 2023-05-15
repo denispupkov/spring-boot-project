7a6f18e325771f7981fba0d41e8151d1b9643cc2
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2023-05-15 14:48:40 +0200

    the kemail/FixNeeded class needs to be fixed.

---
+++ src/main/java/com/cydeo/kemail/FixNeeded.java
@@ -0,0 +1,8 @@
+package com.cydeo.kemail;
+
+public class FixNeeded {
+    public static void main(String[] args) {
+        //todo the next call needs to be fixed by adding your name to the beginning instead of dotS.
+        System.out.println(".... good morning and have a nice week..");
+    }
+}
